e585cb6c9c5d8301298f62ca850be1abbf5d0742
narsnars <nars@kslab.co.kr>
2022-07-18 00:59:04 +0900

    feat:[num-2] Card 생성

---
+++ src/main/java/com/example/gitex/card/Card.java
@@ -0,0 +1,13 @@
+import com.example.gitex.user.User;
+
+public class Card {
+    private String cardCompany;
+    private String cardNumber;
+    private User owner;
+
+    public Card(String cardCompany, String cardNumber, User owner) {
+        this.cardCompany = cardCompany;
+        this.cardNumber = cardNumber;
+        this.owner = owner;
+    }
+}
