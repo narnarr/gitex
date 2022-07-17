1fcf09a17feae32ce43bfa661de7b45bc6ea9c00
narsnars <nars@kslab.co.kr>
2022-07-18 01:21:10 +0900

    feat:[num-4] History 생성

---
+++ src/main/java/com/example/gitex/history/History.java
@@ -0,0 +1,14 @@
+package com.example.gitex.history;
+
+import com.example.gitex.card.Card;
+import com.example.gitex.market.Market;
+import com.example.gitex.user.User;
+
+import java.time.LocalDateTime;
+
+public class History {
+    private Market market;
+    private User user;
+    private Card card;
+    private LocalDateTime createdAt;
+}
