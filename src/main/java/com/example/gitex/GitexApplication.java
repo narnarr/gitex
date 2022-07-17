cf949f7353810f3e1607f90eabf6051e1bfdc493
narsnars <nars@kslab.co.kr>
2022-07-18 00:48:24 +0900

    initial commit

---
+++ src/main/java/com/example/gitex/GitexApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitex/GitexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
